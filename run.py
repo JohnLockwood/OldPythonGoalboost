8a99db3243173d983f5dee9e66b586ffe23089a5
John Lockwood <elitepropertiesbroker@gmail.com>
2015-07-24 18:43:01 -0700

    Replace static stub with a Jinja template, more or less.

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 from eve import Eve
+from flask import render_template
+
 app = Eve(__name__)
 
 @app.route('/')
 def index():
-    return app.send_static_file('test.html')
+    # return app.send_static_file('index.html') -- use this if in static dir
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
